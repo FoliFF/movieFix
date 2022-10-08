3333d95344e9cc4087aad208b65ecc794a6628c9
FoliFF <olof@hisham.se>
2022-10-08 18:32:55 +0200

    Update api for get users

--- movie_api/index.js
+++ movie_api/index.js
@@ -111,7 +111,17 @@ app.get('/users', passport.authenticate('jwt', { session: false}), (req, res) =>
     console.error(err);
     res.status(500).send('Error: ' + err);
   });  
-}); 
+});
+
+app.get('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
+  Users.findOne({ Username: req.params.Username }).then((users) => {
+    res.status(200).json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });  
+});
 
 /*** UPDATE USERS ***/
 app.put('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
